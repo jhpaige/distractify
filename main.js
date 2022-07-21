63d1e6f42880be2a5819d9271f1fc1dc9605d465
jhpaige <jhpaige@usc.edu>
2022-07-20 20:22:30 -0700

    Working for local index

---
+++ main.js
@@ -0,0 +1,101 @@
+// Variable to store random websites
+var sitesList = [
+    "https://longdogechallenge.com/",
+    "https://checkboxrace.com/",
+    "https://onesquareminesweeper.com/",
+    "http://heeeeeeeey.com/",
+    "http://corndog.io/",
+    "https://binarypiano.com/",
+    "https://mondrianandme.com/",
+    "https://puginarug.com",
+    "http://floatingqrcode.com/",
+    "https://checkboxolympics.com/",
+    "https://alwaysjudgeabookbyitscover.com",
+    "https://cant-not-tweet-this.com/",
+    "https://cursoreffects.com",
+    "http://eelslap.com/",
+    "http://www.staggeringbeauty.com/",
+    "http://burymewithmymoney.com/",
+    "https://smashthewalls.com/",
+    "https://jacksonpollock.org/",
+    "http://endless.horse/",
+    "http://drawing.garden/",
+    "https://www.trypap.com/",
+    "http://www.republiquedesmangues.fr/",
+    "http://www.movenowthinklater.com/",
+    "http://www.rrrgggbbb.com/",
+    "http://www.koalastothemax.com/",
+    "http://www.everydayim.com/",
+    "http://randomcolour.com/",
+    "http://cat-bounce.com/",
+    "http://chrismckenzie.com/",
+    "https://thezen.zone/",
+    "http://ninjaflex.com/",
+    "http://ihasabucket.com/",
+    "http://corndogoncorndog.com/",
+    "http://www.hackertyper.com/",
+    "https://pointerpointer.com",
+    "http://imaninja.com/",
+    "http://www.partridgegetslucky.com/",
+    "http://www.ismycomputeron.com/",
+    "http://www.nullingthevoid.com/",
+    "http://www.yesnoif.com/",
+    "http://lacquerlacquer.com",
+    "http://potatoortomato.com/",
+    "http://iamawesome.com/",
+    "https://strobe.cool/",
+    "http://thisisnotajumpscare.com/",
+    "http://doughnutkitten.com/",
+    "http://crouton.net/",
+    "http://corgiorgy.com/",
+    "http://www.wutdafuk.com/",
+    "http://unicodesnowmanforyou.com/",
+    "http://chillestmonkey.com/",
+    "http://scroll-o-meter.club/",
+    "http://www.crossdivisions.com/",
+    "http://tencents.info/",
+    "https://boringboringboring.com/",
+    "http://www.patience-is-a-virtue.org/",
+    "http://pixelsfighting.com/",
+    "http://isitwhite.com/",
+    "https://existentialcrisis.com/",
+    "http://onemillionlols.com/",
+    "http://www.omfgdogs.com/",
+    "http://chihuahuaspin.com/",
+    "http://www.blankwindows.com/",
+    "http://tunnelsnakes.com/",
+    "http://www.trashloop.com/",
+    "http://spaceis.cool/",
+    "http://www.doublepressure.com/",
+    "http://www.donothingfor2minutes.com/",
+    "http://buildshruggie.com/",
+    "http://buzzybuzz.biz/",
+    "http://yeahlemons.com/",
+    "http://wowenwilsonquiz.com",
+    "https://thepigeon.org/",
+    "http://notdayoftheweek.com/",
+    "http://www.amialright.com/",
+    "https://greatbignothing.com/",
+    "https://zoomquilt.org/",
+    "https://dadlaughbutton.com/",
+    "https://remoji.com/",
+    "http://papertoilet.com/",
+    "https://www.bouncingdvdlogo.com/",
+    "https://findtheinvisiblecow.com/"
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    // select all anchor links on page and save to a variable
+    const links = document.getElementsByTagName('a');
+    // iterate through the anchor elements and set href attribute for each to a random website from the array
+    for (let i = 0; i < links.length; i++) {
+        links[i].setAttribute("href", randomURL());
+        console.log(i);
+    }
+})
+
+// helper function to select random element from array
+function randomURL() {
+    const index = Math.floor(Math.random() * sitesList.length);
+    return sitesList[index];
+}
